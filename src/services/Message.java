2060e6d7f4b6e9a1cc07eef0b044ccccb55aac15
Valentin C <valentindu64@hotmail.fr>
2015-10-22 09:06:39 +0200

    New Message file

---
+++ src/services/Message.java
@@ -0,0 +1,43 @@
+package services;
+
+import java.io.Serializable;
+
+/**
+ * Created by ValentinC on 22/10/2015.
+ *
+ */
+public class Message implements Serializable {
+
+    private static enum header_type{
+        hello, helloAck, bye, message
+    }
+
+    private header_type header;
+    private String data;
+
+    private Message(header_type header,String message){
+        this.header=header;
+        this.data = message;
+    }
+
+    private Message(header_type header){
+        this.header=header;
+        this.data = null;
+    }
+
+    public static Message createHello(String nickname){
+        return new Message(header_type.hello, nickname);
+    }
+
+    public static Message createBye(){
+        return new Message(header_type.bye);
+    }
+
+    public static Message createHelloAck(String nickname){
+        return new Message(header_type.helloAck,nickname);
+    }
+
+    public static Message createMessage(String message){
+        return new Message(header_type.message,message);
+    }
+}
